6cbd289928ceb75f82715e4402752ff182fc91f8
MiguelDelgado27 <miguel.9dc@gmail.com>
2018-11-20 00:33:09 -0500

    test

--- src/main/java/com/NutriCoach/controller/ProductController.java
+++ src/main/java/com/NutriCoach/controller/ProductController.java
@@ -3,6 +3,7 @@ package com.NutriCoach.controller;
 import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import javax.validation.Valid;
 
@@ -10,8 +11,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -52,7 +56,7 @@ public class ProductController {
 			products= productService.findAll();
 			return new ResponseEntity<List<Product>>(products,HttpStatus.OK);
 		} catch (Exception e) {
-			return new ResponseEntity<List<User>>(HttpStatus.INTERNAL_SERVER_ERROR);
+			return new ResponseEntity<List<Product>>(HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
 	
--- src/main/java/com/NutriCoach/controller/UserController.java
+++ src/main/java/com/NutriCoach/controller/UserController.java
@@ -3,6 +3,7 @@ package com.NutriCoach.controller;
 import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import javax.validation.Valid;
 
@@ -10,8 +11,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -79,10 +83,10 @@ public class UserController {
 			return new ResponseEntity<Object>(HttpStatus.OK);
 		} catch (Exception e) {
 			return new ResponseEntity<Object>(HttpStatus.INTERNAL_SERVER_ERROR);
-		}
-
+			}
 	}
 
+	
 	@ApiOperation("Eliminar usuario por id")
 	@DeleteMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<String> delete(@PathVariable("id") Integer id) {
